2677cb1755c535defc005cff8a595702cfc44eaf
nikhilkrdwivedi <developer.nikhil@outlook.com>
2024-06-23 11:24:54 +0530

    tap on

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { useEffect, useState } from 'react';
 import './index.css';
-import Arrow from './icons/Arrow';
-import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
 import BottomNavigation from './components/BottomNavigation';
 import TopNavigation from './components/TopNavigation';
 import MainBody from './components/MainBody';
 
 const App = () => {
-  const [points, setPoints] = useState(29857775);
-  const [energy, setEnergy] = useState(2532);
-  const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
-  const pointsToAdd = 12;
-  const energyToReduce = 12;
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (energy - energyToReduce < 0) {
-      return;
-    }
-    const rect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    setPoints(points + pointsToAdd);
-    setEnergy(energy - energyToReduce < 0 ? 0 : energy - energyToReduce);
-    setClicks([...clicks, { id: Date.now(), x, y }]);
-  };
-
-  const handleAnimationEnd = (id: number) => {
-    setClicks((prevClicks) => prevClicks.filter(click => click.id !== id));
-  };
-
-  // useEffect hook to restore energy over time
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setEnergy((prevEnergy) => Math.min(prevEnergy + 1, 6500));
-    }, 100); // Restore 10 energy points every second
-
-    return () => clearInterval(interval); // Clear interval on component unmount
-  }, []);
-
   return (
     <div className="bg-gradient-to-t from-slate-700 to-slate-800 min-h-screen px-4 flex flex-col items-center text-white font-medium">
       <TopNavigation />
