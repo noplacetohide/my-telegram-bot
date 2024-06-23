346be8fa2f406321c0f7411a9c133a6d07fb164e
nikhilkrdwivedi <developer.nikhil@outlook.com>
2024-06-23 11:19:56 +0530

    tap on

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import TelegramInit from './hooks/TelegramInit.tsx'
 import { TelegramProvider } from './context/TelegramContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
